fba297b7a5c3172eb6cb6b02f407bfd7767b7edd
José André de Moura Brito <jambrito@gmail.com>
2017-04-29 07:56:44 -0300

    initial files upload

---
+++ MR_kmeans.R
@@ -0,0 +1,12 @@
+mr_kmeans <- function(dat, k) {
+	result <- matrix(NA, 
+	nrow = nrow(dat), 
+	ncol = length(k), 
+	dimnames = list(NULL, paste0('k=', k)))
+	
+	for (i in 1:length(k)) {
+		result[,i] <- kmeans(dat, k[i])$cluster
+	}
+	
+	return(result)
+}
